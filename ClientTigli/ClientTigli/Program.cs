cd3fbb150c309341accbf5ae8a4417a96a823ed5
Thomas Jalabert <thom.jalabert@gmail.com>
2017-05-12 16:27:30 +0200

    add ClientTigli

---
+++ ClientTigli/ClientTigli/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTigli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceTigli.TigliServiceClient tigliClient = new ServiceTigli.TigliServiceClient();
+            Console.WriteLine(tigliClient.echoget("Paris", "Grenoble"));
+
+            Console.ReadKey();
+        }
+    }
+}
